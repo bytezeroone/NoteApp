a85b843be48b346310110ad40bcf212cc518ae77
bytezeroone <bytezeroone@gmail.com>
2022-05-28 15:18:11 +0400

    style text

--- app/src/main/java/com/bytezeroone/noteapp/presentation/note_listings/NoteItem.kt
+++ app/src/main/java/com/bytezeroone/noteapp/presentation/note_listings/NoteItem.kt
@@ -1,15 +1,19 @@
 package com.bytezeroone.noteapp.presentation.note_listings
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bytezeroone.noteapp.data.local.Note
@@ -20,6 +24,24 @@ fun NoteItem(
     onEvent: (NoteListEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    var isExpanded by remember { mutableStateOf(false) }
+    val textLayoutResultState = remember { mutableStateOf<TextLayoutResult?>(null) }
+    var isClickable by remember { mutableStateOf(false) }
+    var finalText by remember { mutableStateOf(note.description) }
+
+    val textLayoutResult = textLayoutResultState.value
+    LaunchedEffect(textLayoutResult) {
+        if (textLayoutResult == null) return@LaunchedEffect
+
+        when {
+            isExpanded -> {
+                finalText = "${note.description}"
+            }
+            !isExpanded && textLayoutResult.hasVisualOverflow -> {
+                isClickable = true
+            }
+        }
+    }
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
@@ -34,7 +56,7 @@ fun NoteItem(
                 Text(
                     text = note.title,
                     fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
                 )
                 Spacer(modifier = Modifier.width(8.dp))
                 IconButton(onClick = {
@@ -46,9 +68,19 @@ fun NoteItem(
                     )
                 }
             }
-            note.description?.let {
+            finalText.let {
                 Spacer(modifier = Modifier.height(8.dp))
-                Text(text = it)
+                if (it != null) {
+                    Text(
+                        text = it,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 3,
+                        onTextLayout = { textLayoutResultState.value = it },
+                        modifier = modifier
+                            .clickable(enabled = isClickable) { isExpanded = !isExpanded }
+                            .animateContentSize(),
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
             }
         }
     }
